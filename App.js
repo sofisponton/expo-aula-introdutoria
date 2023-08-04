abcf75b5938336687d601febcee67a49d76a6397
Sponton <134696651+sofisponton@users.noreply.github.com>
2023-08-04 11:37:57 +0000

    segundo commit

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Inter-Black': require('./assets/fonts/Inter-Black.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <View><Text>Não carregou a fonte!</Text></View>;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>2º Informática</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontFamily: 'Inter-Black',
+    fontSize: 30
   },
 });
